a76f3b1451fa528ce6625647ee47d8c7d873f7a1
pranjalhinge <pranjalhinge4@gmail.com>
2024-10-30 20:24:14 +0530

    add alt

--- src/components/Hovercard/Hovercard.js
+++ src/components/Hovercard/Hovercard.js
@@ -4,15 +4,15 @@ import Img1 from './HoverImg/note baground.jpg'
 function hovercard({title,imgchar}) {
   return (
     <div className="shop-container">
-      <a href="#" alt="god of war" target="_blank">
+      <a href="#" alt="note" target="_blank">
         <div className="card">
           <div className="wrapper">
-            <img src={Img1} className="cover-image"/>
+            <img src={notebaground} className="cover-image"/>
           
           </div>
           {/* <img src={Img3} class="title"/> */}
           <h4 className='addnotes title'>{title}</h4> 
-          <img src={imgchar} className="character"/>
+          <img src={notebagroundchar} className="character"/>
          </div>
       </a>
   </div>
